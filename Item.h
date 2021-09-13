4c55ec649bc09adccf91fc2ecffa6f531c30d57a
it21029318 <87959447+IT21029318@users.noreply.github.com>
2021-09-13 07:18:22 +0000

    complete tutorial 7

--- Item.h
+++ Item.h
@@ -7,11 +7,15 @@ class Item {
       float discount; // out of 100 e.g. discount = 15
    public:
       // 1. Write the Default Constructor
+      Item();
       // 2. Write the Overloaded Constructor
+      Item(int icode,float price);
       // 3. Write the Destructor
+      ~Item();
 
       void setDiscount(float punitPrice);
       float getDiscount();
       float discountedPrice();
       void display();
 };
+© 2021 GitHub, Inc.
